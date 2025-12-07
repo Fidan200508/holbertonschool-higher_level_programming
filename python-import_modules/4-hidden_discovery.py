6192cac4540d1d14fbcf9781ef1eda5f59e041ba
Fidan200508 <allahverdiyeva200508@gmail.com>
2025-12-07 15:25:05 +0000

    4. Who are you? - hidden discovery task completed

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    filtered_names = []
+
+    for name in names:
+        if not name.startswith('__'):
+            filtered_names.append(name)
+
+    filtered_names.sort()
+
+    for name in filtered_names:
+        print(name)
