7851db27a52d961e733995cb5d0987b1bc6ae0fa
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-12 00:12:52 +0400

    Fix pycodestyle formatting

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 
 if number < 0:
     last_digit = (-number) % 10
-    last_digit= -last_digit
+    last_digit = -last_digit
 else:
     last_digit = number % 10
 
@@ -13,4 +13,7 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(
+        f"Last digit of {number} is {last_digit} "
+        "and is less than 6 and not 0"
+    )
