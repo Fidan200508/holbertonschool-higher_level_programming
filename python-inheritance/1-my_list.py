5e5fe4b6084c014e385c2203c71d98ad133f3a20
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-17 00:54:25 +0000

    Fix PEP8 and docstring formatting for MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-This module defines the MyList class.
-"""
+"""This module defines the MyList class."""
 
 
 class MyList(list):
-    """A class that inherits from list"""
+    """A class that inherits from list."""
 
     def print_sorted(self):
-        """Prints the list in ascending order
-         without modifying the original list"""
+        """Print the list in ascending order without modifying it."""
         print(sorted(self))
-
