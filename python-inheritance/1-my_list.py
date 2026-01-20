8b0864518fc2981924332f2d185ac608592d3519
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-20 19:10:03 +0000

    Add MyList class and tests

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""This module defines the MyList class."""
+"""
+This module defines the MyList class.
+"""
 
 
 class MyList(list):
-    """A class that inherits from list."""
+    """A class that inherits from list"""
 
     def print_sorted(self):
-        """Print the list in ascending order without modifying it."""
+        """Prints the list in ascending order
+         without modifying the original list"""
         print(sorted(self))
