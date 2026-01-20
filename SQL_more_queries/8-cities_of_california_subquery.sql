22b19d2551a3e20329f21132764157b1dbbeb8a3
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-01-20 17:46:08 +0000

    Add subquery to list cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- a script that lists all the cities of California that can be found in the database hbtn_0d_usa
+-- a script that lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT id,
+       name
+FROM cities c
+    WHERE c.state_id = (SELECT s.id
+                        FROM states s WHERE s.name = "California")
+ORDER BY c.id;
